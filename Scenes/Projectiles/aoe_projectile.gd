c7f55478225f8e24ee9a6a3ffd9488f5b0395503
Jack Lewis <102907496+GiantHotdog@users.noreply.github.com>
2026-05-26 12:49:18 +0100

    made aoe projectiles pick new target if current target is freed

--- Scenes/Projectiles/aoe_projectile.gd
+++ Scenes/Projectiles/aoe_projectile.gd
@@ -4,7 +4,7 @@ extends BaseProjectile
 @export var explode_final_scale:float = 20
 
 @onready var AoeArea:Area2D = $AoeArea
-@onready var Area:Area2D = $Area2D
+@onready var tracking_area:Area2D = $TrackArea
 
 var is_exploding:bool = false
 
@@ -16,6 +16,22 @@ func _process(delta: float) -> void:
 		var move_vector = Vector2.UP.rotated(rotation) * move_speed * delta
 		position += move_vector
 	elif not is_exploding:
+		# If the target no longet exists, attempt to find a new one
+		var closest_node:BaseEnemy = null
+		var closest_node_distance:float
+		for child in tracking_area.get_overlapping_areas():
+			var parent = child.get_parent()
+			if parent is BaseEnemy:
+				# if there is currently no node, just fill in the basic one to avoid a crash by accessing its parameters later
+				if not closest_node:
+					closest_node = parent
+					closest_node_distance = global_position.distance_to(closest_node.global_position)
+				elif closest_node_distance > global_position.distance_to(parent.global_position):
+					closest_node = parent
+					closest_node_distance = global_position.distance_to(closest_node.global_position)
+		target = closest_node
+	if not target and not is_exploding:
+		# If no target has been found, remove the projectile
 		queue_free()
 
 
