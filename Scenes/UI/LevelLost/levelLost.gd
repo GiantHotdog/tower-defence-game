83bd4740ba98e933b5342062a1e3876b818bed25
Jack Lewis <102907496+GiantHotdog@users.noreply.github.com>
2026-06-06 16:26:02 +0100

    changed main scene to new animation

--- Scenes/UI/LevelLost/levelLost.gd
+++ Scenes/UI/LevelLost/levelLost.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/UI/Menu/menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/UI/GRUBMenu/new_menu.tscn")
