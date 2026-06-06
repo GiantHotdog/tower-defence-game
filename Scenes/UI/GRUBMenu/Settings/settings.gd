d946448c0311c6d08039257a80e8cb451e81697a
Jack Lewis <102907496+GiantHotdog@users.noreply.github.com>
2026-06-06 21:35:24 +0100

    disabled settings options with no associated pages

--- Scenes/UI/GRUBMenu/Settings/settings.gd
+++ Scenes/UI/GRUBMenu/Settings/settings.gd
@@ -3,18 +3,18 @@ extends GRUB
 
 func on_menu_select_pressed():
 	match currently_selected:
-		0:
-			play_outro()
-			await outro_finished
-			get_tree().change_scene_to_file("res://Scenes/UI/GRUBMenu/Grub.tscn")
+		#0:
+			#play_outro()
+			#await outro_finished
+			#get_tree().change_scene_to_file("res://Scenes/UI/GRUBMenu/Grub.tscn")
 		1:
 			play_outro()
 			await outro_finished
 			get_tree().change_scene_to_file("res://Scenes/UI/GRUBMenu/Settings/AudioSettings/audio_settings.tscn")
-		2:
-			play_outro()
-			await outro_finished
-			get_tree().quit()
+		#2:
+			#play_outro()
+			#await outro_finished
+			#get_tree().quit()
 		3:
 			play_outro()
 			await outro_finished
